bf42886f468bc31114a59083d861db5cc070fd7a
andy <andywang0979@gmail.com>
2018-08-16 13:40:19 +0800

    add Edit
    add Edit

--- coop/coop/Controllers/HomeController.cs
+++ coop/coop/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using coop.Models;
@@ -10,7 +13,8 @@ namespace coop.Controllers
     public class HomeController : Controller
     {
         coopEntities2 db = new coopEntities2();
-        
+        private object mb;
+
 
         // GET: andy Home 
         public ActionResult Index()
@@ -19,5 +23,41 @@ namespace coop.Controllers
                         select o;
             return View(query.ToList());
         }
+
+
+        public ActionResult Edit(int? id)
+        {
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            member member = db.members.Find(id);
+            //if (member == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            return View(member);
+        }
+
+        // POST: members/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "personId,name,city")] member member)
+        {
+            //if (ModelState.IsValid)
+            //{
+            db.Entry(member).State = EntityState.Modified;
+            db.SaveChanges();
+                return RedirectToAction("Index");
+            //}
+
+            //return View(member);
+
+        }
+
+
+
+
+
+
     }
 }
